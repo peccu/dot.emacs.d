c798fe8c461f6ad08b610cab8038a9c3deea8fba
peccu <peccul@gmail.com>
2019-02-04 14:06:06 +0900

    add neotree

---
+++ conf.d/neotree.el
@@ -0,0 +1,12 @@
+(require 'neotree)
+(global-set-key (kbd "<f8>") 'neotree-toggle)
+(setq neo-autorefresh t)
+;; (setq neo-confirm-delete-directory-recursively 'off-p)
+;; (setq neo-confirm-kill-buffers-for-files-in-directory 'off-p)
+(setq neo-hidden-regexp-list '("\\.pyc$" "~$" "^#.*#$" "\\.elc$" "\\.o$"))
+;; (setq neo-keymap-style 'concise)
+;; (setq neo-show-slash-for-folder nil)
+;; needs all-the-icons
+(setq neo-theme 'icons)
+;; (setq neo-toggle-window-keep-p t)
+(setq neo-vc-integration '(face))
