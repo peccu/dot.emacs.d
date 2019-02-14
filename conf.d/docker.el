b1dc48136598bb39dcbe1b1d3c6179dba717b2a5
peccu <peccul@gmail.com>
2019-02-14 16:13:53 +0900

    add docker conf

---
+++ conf.d/docker.el
@@ -0,0 +1,6 @@
+(require 'docker)
+(require 'dockerfile-mode)
+(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+(require 'docker-compose-mode)
+(require 'docker-tramp-compat)
+(set-variable 'docker-tramp-use-names t)
