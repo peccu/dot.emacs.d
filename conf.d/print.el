c6fffd820624ffa1d798d3fbe99aeb5cc8218937
peccu <peccul@gmail.com>
2019-02-15 13:34:59 +0900

    add printer config

---
+++ conf.d/print.el
@@ -0,0 +1,9 @@
+(setq ;; ps-print-color-p t
+      ;; ps-lpr-command "gswin32c.exe"
+      ;; ps-multibyte-buffer 'non-latin-printer
+      ;; ps-lpr-switches '("-sDEVICE=mswinpr2" "-dNOPAUSE" "-dBATCH" "-dWINKANJI")
+      ;; printer-name nil
+      ;; ps-printer-name nil
+      ;; ps-printer-name-option nil
+      ps-print-header nil          ; ヘッダの非表示
+      )
