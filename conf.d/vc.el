bd7da57772fad9c3e757bd6a8d72d8c9acb9a2e8
peccu <peccul@gmail.com>
2019-02-26 11:24:28 +0900

    add vc conf

---
+++ conf.d/vc.el
@@ -0,0 +1,4 @@
+;; remove hook
+;; http://stackoverflow.com/questions/5748814/how-does-one-disable-vc-git-in-emacs
+(eval-after-load "vc" '(remove-hook 'find-file-hooks 'vc-find-file-hook))
+(setq vc-handled-backends nil)
