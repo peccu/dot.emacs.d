062f90c4476f15b8e36174892f7fa2181fdbfff2
peccu <peccul@gmail.com>
2019-02-26 11:52:26 +0900

    disable all-the-icons-dired for heavy timers

--- conf.d/all-the-icons.el
+++ conf.d/all-the-icons.el
@@ -1,4 +1,5 @@
 (require 'all-the-icons)
+(setq inhibit-compacting-font-caches t)
 ;; M-x all-the-icons-install-fonts
-(when (require 'all-the-icons-dired nil t)
-  (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
+;; (when (require 'all-the-icons-dired nil t)
+;;   (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
