5efe00521dfee58f0cd7a5c3559ca31901e423f5
peccu <peccul@gmail.com>
2019-08-29 10:53:27 +0900

    update package config

--- conf.d/0package.el
+++ conf.d/0package.el
@@ -1,18 +1,11 @@
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-;;              '("melpa" . "https://elpa.zilongshanren.com/"))
-;;              '("melpa" . "https://melpa.org/packages/"))
-;; (add-to-list 'package-archives
-;;              '("marmalade" . "http://marmalade-repo.org/packages/"))
-;; (add-to-list 'package-archives
-;;              '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(package-initialize t)
-;; (setq package-archives '(("marmalade" . "http://marmalade-repo.org/packages/")
-;;                          ("gnu" . "http://elpa.gnu.org/packages/")
-;;                          ;;  ("melpa" . "https://elpa.zilongshanren.com/")
-;;                          ;; ("melpa" . "mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-;;                          ("melpa" . "https://melpa.org/packages/")
-;;                          ;; ("melpa-stable" . "https://stable.melpa.org/packages/")
-;;                          ))
+(prog1 "setup package"
+  (custom-set-variables
+   '(package-archives '(("org"   . "https://orgmode.org/elpa/")
+                        ("melpa" . "https://melpa.org/packages/")
+                        ("gnu"   . "https://elpa.gnu.org/packages/")))
+   ;; https://www.reddit.com/r/emacs/comments/cdei4p/failed_to_download_gnu_archive_bad_request/etw48ux
+   '(gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
+  (package-initialize)
+  ;; (package-refresh-contents)
+  )
