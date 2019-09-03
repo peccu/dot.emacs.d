e77a2e8a3782ba5b6f24a4e4a809d73b5f090b08
peccu <peccul@gmail.com>
2019-09-03 16:34:38 +0900

    add window-numbering

---
+++ boot-mac/window-numbering.el
@@ -0,0 +1,1 @@
+../conf.d/window-numbering.el
---
+++ conf.d/window-numbering.el
@@ -0,0 +1,4 @@
+(require 'window-numbering)
+;; (setq window-numbering-assign-func
+;;       (lambda () (when (equal (buffer-name) "*Calculator*") 9)))
+(window-numbering-mode t)
