22165894bc57711cc72ca8e3e788ef9193c08df8
peccu <peccul@gmail.com>
2019-09-03 16:34:56 +0900

    add windmove

---
+++ conf.d/windmove.el
@@ -0,0 +1,10 @@
+;; ;; move splited frame
+;; ;; Shift-(left|right|up|down)
+(windmove-default-keybindings)
+(setq windmove-wrap-around t)
+(defun counter-other-window ()
+  (interactive)
+  (other-window -1))
+(global-set-key (kbd "C-;") 'other-window)
+;; (global-set-key (kbd "C-:") 'counter-other-window)
+(global-set-key (kbd "C-M-;") 'counter-other-window)
