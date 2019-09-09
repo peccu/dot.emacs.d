2cd9006bc260d8581b97414798924c6f6809a099
peccu <peccul@gmail.com>
2019-09-09 15:06:32 +0900

    add auto highlight symbol

---
+++ boot-mac/auto-highlight-symbol-mode.el
@@ -0,0 +1,1 @@
+../conf.d/auto-highlight-symbol-mode.el
---
+++ conf.d/auto-highlight-symbol-mode.el
@@ -0,0 +1,9 @@
+(require 'auto-highlight-symbol)
+(ahs-set-idle-interval 0.5)
+(global-auto-highlight-symbol-mode t)
+;; 適用範囲をデフォルトの見えている範囲ではなく buffer 全体に
+;; https://github.com/shishi/.emacs.d/blob/master/inits/20-auto-highlight-symbol-mode.el
+(custom-set-variables '(ahs-default-range (quote ahs-range-whole-buffer)))
+
+;; フックは各モードの設定ファイルに記述
+;; (add-hook 'coffee-mode-hook 'auto-highlight-symbol-mode)
