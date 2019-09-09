127631708a79a27ac96816c357f34e94d7a5d1c8
peccu <peccul@gmail.com>
2019-09-09 15:08:05 +0900

    add buffer's config

---
+++ boot-mac/buffer.el
@@ -0,0 +1,1 @@
+../conf.d/buffer.el
---
+++ conf.d/buffer.el
@@ -0,0 +1,15 @@
+;; バッファ画面外文字の切り詰め表示
+(setq truncate-lines nil)
+
+;; ウィンドウ縦分割時のバッファ画面外文字の切り詰め表示
+(setq truncate-partial-width-windows t)
+
+;; 同一バッファ名にディレクトリ付与
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+(setq uniquify-ignore-buffers-re "*[^*]+*")
+
+;; バッファ一覧でソートする
+(define-key Buffer-menu-mode-map (kbd "S") 'Buffer-menu-sort)
+;; (insert "\n" (substitute-command-keys "\\{Buffer-menu-mode-map}"))
