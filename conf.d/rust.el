edad217575a19ca6fb81f9da40047c8de7255dde
peccu <peccul@gmail.com>
2019-09-30 11:49:50 +0900

    add rust

---
+++ conf.d/rust.el
@@ -0,0 +1,9 @@
+(require 'company)
+(add-hook 'racer-mode-hook #'company-mode)
+
+(require 'rust-mode)
+(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
