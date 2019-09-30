e1d739717aede1d00e27c2600612596719e151c2
peccu <peccul@gmail.com>
2019-09-30 11:50:34 +0900

    add plantuml

---
+++ conf.d/plantuml.el
@@ -0,0 +1,10 @@
+;; (setq plantuml-jar-path "~/bin/plantuml.jar")
+(setq plantuml-jar-path nil)
+(add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
+;; M-x plantuml-complete-symbol
+;; M-x plantuml-preview
+;; C-c C-c  plantuml-preview: renders a PlantUML diagram from the current buffer in the best supported format
+;; C-u C-c C-c  plantuml-preview in other window
+;; C-u C-u C-c C-c plantuml-preview in other frame
+(add-to-list
+  'org-src-lang-modes '("plantuml" . plantuml))
