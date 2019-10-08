8cd8649cad91bb770fe042b432af6699c963b048
peccu <peccul@gmail.com>
2019-10-08 18:07:12 +0900

    change indent size for nxml mode

--- conf.d/nxml.el
+++ conf.d/nxml.el
@@ -1,5 +1,7 @@
 (add-to-list 'auto-mode-alist '("\\.tpl\\'" . nxml-mode))
 (add-hook 'nxml-mode-hook
           '(lambda ()
+             (setq nxml-child-indent 2
+                   nxml-attribute-indent 2)
              (define-key nxml-mode-map (kbd "M-}") 'forward-paragraph)
              (define-key nxml-mode-map (kbd "M-{") 'backward-paragraph)))
