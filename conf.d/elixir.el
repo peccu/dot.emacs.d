18c594a0b40241a0247716721bb21553a29afa71
peccu <peccul@gmail.com>
2019-11-14 14:18:01 +0900

    update elixir-mode

--- conf.d/elixir.el
+++ conf.d/elixir.el
@@ -1,3 +1,6 @@
 (require 'elixir-mode)
 (require 'alchemist)
 (add-hook 'elixir-mode-hook 'ac-alchemist-setup)
+(add-hook 'elixir-mode-hook
+          (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
+(add-to-list 'auto-mode-alist '("\\.leex\\'" . elixir-mode))
