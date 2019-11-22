33c5f1267f930a1a0f234b42918bd55be04e1761
peccu <peccul@gmail.com>
2019-11-22 09:20:32 +0900

    add submodule emacs-sourcetrail

---
+++ boot-mac/lsp-python-ms.el
@@ -0,0 +1,1 @@
+../conf.d/lsp-python-ms.el
---
+++ conf.d/lsp-python-ms.el
@@ -0,0 +1,8 @@
+(require 'lsp-python-ms)
+;; docker run --rm -it mcr.microsoft.com/dotnet/core/sdk:3.0
+;; git clone https://github.com/Microsoft/python-language-server.git
+;; cd python-language-server/src/LanguageServer/Impl/
+;; dotnet publish -c Release -r osx-x64
+;; tar czf python-lsp.tar.gz output/bin/Release/osx-x64/
+(setq lsp-python-ms-executable
+      "~/bin/python-language-server/output/bin/Release/osx-x64/publish/Microsoft.Python.LanguageServer")
