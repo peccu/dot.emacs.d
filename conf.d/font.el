3aff2b56e0990dae70815874961758f49a45574f
peccu <peccul@gmail.com>
2020-01-10 12:41:28 +0900

    update font conf

--- conf.d/font.el
+++ conf.d/font.el
@@ -1,24 +1,28 @@
-;; 標準フォントの設定
-;; (set-default-font "M+2VM+IPAG circle-12")
+;; ;; 標準フォントの設定
+;; ;; (set-default-font "M+2VM+IPAG circle-12")
 
-;; IME変換時フォントの設定（テストバージョンのみ）
-;; (setq w32-ime-font-face "MigMix 1M")
-;; (setq w32-ime-font-height 22)
+;; ;; IME変換時フォントの設定（テストバージョンのみ）
+;; ;; (setq w32-ime-font-face "MigMix 1M")
+;; ;; (setq w32-ime-font-height 22)
 
-;; 固定等幅フォントの設定
-;; (set-face-attribute 'fixed-pitch    nil :family "M+2VM+IPAG circle")
+;; ;; 固定等幅フォントの設定
+;; ;; (set-face-attribute 'fixed-pitch    nil :family "M+2VM+IPAG circle")
 
-;; 可変幅フォントの設定
-;; (set-face-attribute 'variable-pitch nil :family "M+2VM+IPAG circle")
+;; ;; 可変幅フォントの設定
+;; ;; (set-face-attribute 'variable-pitch nil :family "M+2VM+IPAG circle")
 
-;; http://blog.livedoor.jp/tek_nishi/archives/8590439.html
-;; (set-face-attribute 'default nil :family "Menlo" :height 120)
-;; (set-face-attribute 'default nil :family "Fira Code" :height 120)
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0208
-                  (font-spec :family "Hiragino Kaku Gothic ProN"))
-(add-to-list 'face-font-rescale-alist
-             '(".*Hiragino Kaku Gothic ProN.*" . 1.2))
+;; ;; http://blog.livedoor.jp/tek_nishi/archives/8590439.html
+;; ;; (set-face-attribute 'default nil :family "Menlo" :height 120)
+;; ;; (set-face-attribute 'default nil :family "Fira Code" :height 120)
+;; (set-fontset-font (frame-parameter nil 'font)
+;;                   'japanese-jisx0208
+;;                   ;; (font-spec :family "Hiragino Maru Gothic ProN"))
+;;                   (font-spec :family "Hiragino Mincho ProN"))
+;;                   ;; (font-spec :family "Hiragino Kaku Gothic ProN"))
+;; (add-to-list 'face-font-rescale-alist
+;;              ;; '(".*Hiragino Maru Gothic ProN.*" . 1.2))
+;;              '(".*Hiragino Mincho ProN.*" . 1.2))
+;;              ;; '(".*Hiragino Kaku Gothic ProN.*" . 1.2))
 
 ;; https://github.com/tonsky/FiraCode/wiki/Emacs-instructions
 (when (window-system)
@@ -79,3 +83,47 @@
 ;;                     :height 130
 ;;                     :weight 'normal
 ;;                     :width 'normal)
+
+(when (eq system-type 'darwin)
+
+  (face-all-attributes 'default)
+
+  ;; default Latin font (e.g. Consolas)
+  (set-face-attribute 'default nil :family "Fira Code")
+  ;; (set-face-attribute 'default nil :family "YuKyokasho Yoko")
+
+  ;; default font size (point * 10)
+  ;;
+  ;; WARNING!  Depending on the default font,
+  ;; if the size is not supported very well, the frame will be clipped
+  ;; so that the beginning of the buffer may not be visible correctly.
+  (set-face-attribute 'default nil :height 120)
+
+  ;; use specific font for Korean charset.
+  ;; if you want to use different font size for specific charset,
+  ;; add :size POINT-SIZE in the font-spec.
+  ;; japanese-jisx0208	                        2  94 B
+  ;; japanese-jisx0208-1978	                        2  94 @
+  ;; japanese-jisx0212	                        2  94 D
+  ;; japanese-jisx0213-1	                        2  94 O
+  ;; japanese-jisx0213-2	                        2  94 P
+  ;; japanese-jisx0213.2004-1	                2  94 Q
+  ;; jisx0201	                                1 224 none
+  ;; (set-fontset-font t 'japanese-jisx0208 (font-spec :name "Hiragino Mincho ProN" :size 11))
+  ;; (set-fontset-font t 'japanese-jisx0208 (font-spec :name "Hiragino Maru Gothic ProN" :size 11))
+  ;; (set-fontset-font t 'japanese-jisx0208 (font-spec :name "Hiragino Kaku Gothic ProN" :size 11))
+  (set-fontset-font t 'japanese-jisx0208 (font-spec :name "Osaka" :size 12))
+  ;; (set-fontset-font t 'japanese-jisx0208 (font-spec :name "Hiragino Kaku Gothic ProN" :size 12))
+  ;; (set-fontset-font t 'japanese-jisx0208 (font-spec :name "Tsukushi A Round Gothic" :size 14))
+  ;; (set-fontset-font t 'ascii (font-spec :name "Fira Code" :size 18))
+  ;; (set-fontset-font t 'japanese-jisx0208 (font-spec :name "YuKyokasho Yoko" :size 14))
+
+  ;; (set-fontset-font t 'japanese-jisx0208 (font-spec :name "Hannotate SC" :size 11))
+  ;; (set-fontset-font t 'japanese-jisx0208 (font-spec :name "Songti SC" :size 12))
+  ;; (set-fontset-font t 'japanese-jisx0208 (font-spec :name "STFangsong" :size 12))
+  ;; (set-fontset-font t 'japanese-jisx0208 (font-spec :name "STKaiti" :size 13))
+  ;; (set-fontset-font t 'japanese-jisx0208 (font-spec :name "Weibei SC" :size 13))
+  ;; (set-fontset-font t 'japanese-jisx0208 (font-spec :name "Yuanti SC" :size 13))
+
+  ;; you may want to add different for other charset in this way.
+  )
