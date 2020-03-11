f214def4a77bd51a40c85f0c03500bf22ab841d4
peccu <peccul@gmail.com>
2020-03-11 12:07:30 +0900

    add custom-set

---
+++ conf.d/custom-set.el
@@ -0,0 +1,17 @@
+;; customize の出力先
+;; cf.http://www1.ocn.ne.jp/~ruby11/linux/emacs4.html
+(setq custom-file "~/.emacs.d/conf.d/custom-set.el")
+(when window-system
+  (scroll-bar-mode -1))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+)
