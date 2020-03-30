1f03857f9504b3dc6c86912f586d32070286963b
peccu <peccul@gmail.com>
2020-03-30 14:24:10 +0900

    add zero width spaces to highlight

--- conf.d/highlight-chars.el
+++ conf.d/highlight-chars.el
@@ -4,13 +4,15 @@
 (add-hook 'font-lock-mode-hook 'hc-highlight-tabs)
 (add-hook 'font-lock-mode-hook 'hc-highlight-trailing-whitespace)
 
+;; ---------------- wide space
+
 ;;;###autoload
 (defface hc-wide-space '((t (:background "PaleGreen")))
   "*Face for highlighting wide spaces (`　') in Font-Lock mode."
   :group 'Highlight-Characters :group 'faces)
 
 (defvar hc-highlight-wide-space-p nil
-  "Non-nil means font-lock mode highlights TAB characters (`　').")
+  "Non-nil means font-lock mode highlights wide space characters (`　').")
 
 ;;;###autoload
 (defalias 'toggle-highlight-wide-space 'hc-toggle-highlight-wide-space)
@@ -37,3 +39,39 @@
     (font-lock-remove-keywords nil '(("[　]+" (0 'hc-wide-space t))))))
 
 (add-hook 'font-lock-mode-hook 'hc-highlight-wide-space)
+
+;; ---------------- zero width spaces
+
+;;;###autoload
+(defface hc-zero-width-space '((t (:background "PaleGreen")))
+  "*Face for highlighting zero width spaces (`​') in Font-Lock mode."
+  :group 'Highlight-Characters :group 'faces)
+
+(defvar hc-highlight-zero-width-space-p nil
+  "Non-nil means font-lock mode highlights zero width spaces characters (`​').")
+
+;;;###autoload
+(defalias 'toggle-highlight-zero-width-space 'hc-toggle-highlight-zero-width-space)
+;;;###autoload
+(defun hc-toggle-highlight-zero-width-space (&optional msgp)
+  "Toggle highlighting of zero width spaces, using face `hc-zero-width-space'."
+  (interactive "p")
+  (setq hc-highlight-zero-width-space-p  (not hc-highlight-zero-width-space-p))
+  (if hc-highlight-zero-width-space-p
+      (add-hook 'font-lock-mode-hook 'hc-highlight-zero-width-space)
+    (remove-hook 'font-lock-mode-hook 'hc-highlight-zero-width-space)
+    (hc-dont-highlight-zero-width-space))
+  (font-lock-mode) (font-lock-mode)
+  (when msgp
+    (message "zero width spaces highlighting is now %s" (if hc-highlight-zero-width-space-p "ON" "OFF"))))
+
+(defun hc-highlight-zero-width-space ()
+  "Highlight zero width spaces (`​')."
+  (font-lock-add-keywords nil '(("[​]+" (0 'hc-zero-width-space t))) 'APPEND))
+
+(defun hc-dont-highlight-zero-width-space ()
+  "Do not highlight zero width spaces (`​')."
+  (when (fboundp 'font-lock-remove-keywords)
+    (font-lock-remove-keywords nil '(("[​]+" (0 'hc-zero-width-space t))))))
+
+(add-hook 'font-lock-mode-hook 'hc-highlight-zero-width-space)
