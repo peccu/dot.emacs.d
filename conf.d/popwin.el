962f6ba515d81badf3b33e25d597606afb5048b4
peccu <peccul@gmail.com>
2020-03-30 14:20:59 +0900

    update popwin conf

--- conf.d/popwin.el
+++ conf.d/popwin.el
@@ -8,8 +8,31 @@
 (push '("*helm M-x*" :height 30) popwin:special-display-config)
 (push '("*Codic Result*" :height 30) popwin:special-display-config)
 (push '("*Help*" :height 30) popwin:special-display-config)
-(push '("*quickrun*" :height 30) popwin:special-display-config)
+;; (push '("*quickrun*" :height 30) popwin:special-display-config)
 
 (push '("^\\*ag.*\\*" :regexp t :height 80) popwin:special-display-config)
 (push '("^\\*anything.*\\*$" :regexp t :height 30) popwin:special-display-config)
 (push '("^\\*helm.*\\*$" :regexp t :height 30) popwin:special-display-config)
+
+
+;; (setq popwin:special-display-config
+;;       '(
+;;         ("*Miniedit Help*" :noselect t)
+;;         help-mode
+;;         (completion-list-mode :noselect t)
+;;         (compilation-mode :noselect t)
+;;         (grep-mode :noselect t)
+;;         (occur-mode :noselect t)
+;;         ("*Pp Macroexpand Output*" :noselect t)
+;;         "*Shell Command Output*" "*vc-diff*" "*vc-change-log*" (" *undo-tree*" :width 60 :position right)
+;;         ("^\\*anything.*\\*$" :regexp t)
+;;         "*slime-apropos*"
+;;         "*slime-macroexpansion*"
+;;         "*slime-description*"
+;;         ("*slime-compilation*" :noselect t)
+;;         "*slime-xref*"
+;;         (sldb-mode :stick t)
+;;         slime-repl-mode
+;;         slime-connection-list-mode
+;;         )
+;;       )
