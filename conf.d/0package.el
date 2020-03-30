dfd74c68f206cb87c34bef29ece21d12855b1761
peccu <peccul@gmail.com>
2020-03-30 14:26:37 +0900

    update package conf for newer emacs

--- conf.d/0package.el
+++ conf.d/0package.el
@@ -3,9 +3,14 @@
   (custom-set-variables
    '(package-archives '(("org"   . "https://orgmode.org/elpa/")
                         ("melpa" . "https://melpa.org/packages/")
-                        ("gnu"   . "https://elpa.gnu.org/packages/")))
-   ;; https://www.reddit.com/r/emacs/comments/cdei4p/failed_to_download_gnu_archive_bad_request/etw48ux
-   '(gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
+                        ("gnu"   . "https://elpa.gnu.org/packages/"))))
+
+  (when (and (version<  "25" emacs-version)
+             (version< emacs-version "26.3"))
+    (custom-set-variables
+     ;; https://www.reddit.com/r/emacs/comments/cdei4p/failed_to_download_gnu_archive_bad_request/etw48ux
+     '(gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
+    )
   (package-initialize)
   ;; (package-refresh-contents)
   )
