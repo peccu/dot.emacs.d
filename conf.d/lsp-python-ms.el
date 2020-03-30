68691c0b002016085c26b14659cde9d6c5c90721
peccu <peccul@gmail.com>
2020-03-30 14:24:40 +0900

    add python lsp

--- conf.d/lsp-python-ms.el
+++ conf.d/lsp-python-ms.el
@@ -6,3 +6,6 @@
 ;; tar czf python-lsp.tar.gz output/bin/Release/osx-x64/
 (setq lsp-python-ms-executable
       "~/bin/python-language-server/output/bin/Release/osx-x64/publish/Microsoft.Python.LanguageServer")
+(setq lsp-python-ms-python-executable-cmd "python3")
+(add-hook 'python-mode-hook #'lsp) ; or lsp-deferred
+;; (setq lsp-python-ms-extra-paths '())
