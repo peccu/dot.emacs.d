0bc4f4fb1ed9de3623aca4acb9ccc1c19d97a111
peccu <peccul@gmail.com>
2020-03-30 14:24:59 +0900

    add taskjuggler conf

--- conf.d/org-taskjuggler.el
+++ conf.d/org-taskjuggler.el
@@ -1,5 +1,11 @@
 ;; ;; add contrib for ox-taskjuggler
+;; ;; with downlowded from https://orgmode.org
 ;; (add-to-list 'load-path "~/.emacs.d/lisp/org/contrib/lisp")
+;; need sudo gem install taskjuggler
+
+;; https://github.com/emacsmirror/org/blob/master/contrib/lisp/ox-taskjuggler.el
+;; https://orgmode.org/worg/exporters/taskjuggler/ox-taskjuggler.html
+;; http://taskjuggler.org/tj3/manual/toc.html
 (eval-after-load "org"
   (progn
     (require 'ox-taskjuggler)
@@ -40,4 +46,31 @@ resourcereport resourceGraph \"\" {
   hidetask ~(isleaf() & isleaf_())
   sorttasks plan.start.up
 }"))
-    t))
+    (setq org-taskjuggler-valid-resource-attributes
+          '(limits vacation shift booking efficiency journalentry rate workinghours flags leaves))
+
+;;     ;; http://blog.ichiroc.in/entry/2010/05/16/000500#
+;;     ;; New function
+;;     (defun org-taskjuggler-get-random-id ()
+;;       (concat "id" (number-to-string (random 100000000))))
+;;     ;; Override
+;;     ;; 追記:2010/05/17
+;;     ;; 元プログラムが task_id property を考慮してないようだったので、
+;;     ;; 考慮するように修正。
+;;     ;; これで BLOCKER 等に task id を指定しても無視されなくなる。
+;;     (defun org-taskjuggler-get-unique-id (item unique-ids)
+;;       "Return a unique id for an ITEM which can be a task or a resource.
+;; The id is derived from the headline and made unique against
+;; UNIQUE-IDS. If the first part of the headline is not unique try to add
+;; more parts of the headline or finally add more underscore characters (\"_\")."
+;;       (let* (
+;;              (task-id (cdr (assoc "task_id" item)))
+;;              (headline (cdr (assoc "headline" item)))
+;;              (parts (split-string headline))
+;;              (id (let *1
+;;                    (t potential-id)))))
+;;       (while (member id unique-ids)
+;;         (setq id (org-taskjuggler-get-random-id)))
+;;       id))
+
+  t))
