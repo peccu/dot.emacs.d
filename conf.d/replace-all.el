a66c9e7424b2388ccacefdb9200c2458e0a72bdd
peccu <peccul@gmail.com>
2023-04-05 09:46:21 +0000

    update replace-all

--- conf.d/replace-all.el
+++ conf.d/replace-all.el
@@ -3,5 +3,13 @@
   "Emacs Lispテクニックバイブル p.111 replace-regexp-in-lisp より"
   (save-excursion
     (goto-char (point-min))
+    (replace-all-from-here regexp to-string)))
+
+(defun replace-all-from-here (regexp to-string)
+  (save-excursion
     (while (re-search-forward regexp nil t)
       (replace-match to-string))))
+
+(defun query-replace-all (from-string to-string)
+  (save-excursion
+    (query-replace from-string to-string nil (point-min) (point-max))))
