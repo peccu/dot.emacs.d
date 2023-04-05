bf74480226ca3ce171f89ba11b437b457ad09abc
peccu <peccul@gmail.com>
2023-04-05 02:21:52 +0000

    fix style and add use-package

--- conf.d/0package.el
+++ conf.d/0package.el
@@ -1,9 +1,12 @@
 (require 'package)
 (prog1 "setup package"
   (custom-set-variables
-   '(package-archives '(("org"   . "https://orgmode.org/elpa/")
+   '(package-archives '(
+                        ("org"   . "https://orgmode.org/elpa/")
                         ("melpa" . "https://melpa.org/packages/")
-                        ("gnu"   . "https://elpa.gnu.org/packages/"))))
+                        ("gnu"   . "https://elpa.gnu.org/packages/")
+                        ;; ("melpa-stable" . "https://stable.melpa.org/packages/")
+                        )))
 
   (when (and (version<  "25" emacs-version)
              (version< emacs-version "26.3"))
@@ -12,5 +15,15 @@
      '(gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
     )
   (package-initialize)
+  ;; elpaディレクトリを消したら再実行必要
   ;; (package-refresh-contents)
   )
+
+(defun require-with-install (package)
+  (interactive)
+  (unless (package-installed-p package)
+    (package-install package))
+  (require package))
+
+;; for version< emacs-version 29
+(require-with-install 'use-package)
