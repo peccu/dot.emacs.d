68945926d66a63dabb02b65d45ad8d6886a34676
peccu <peccul@gmail.com>
2023-08-17 15:37:35 +0900

    merged indent-tabs-nil.el

--- conf.d/indent-tabs-nil.el
+++ conf.d/indent-tabs-nil.el
@@ -1,4 +1,10 @@
-;; タブの代わりにスペースを使う
-(setq indent-tabs-mode nil)
-(setq-default tab-width 8
-              indent-tabs-mode nil)
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  ;; タブの代わりにスペースを使う
+  (setq indent-tabs-mode nil)
+  (setq-default tab-width 8
+                indent-tabs-mode nil)
+  )
