022ac29fd5ea65003e8e2a9576a1f1b761180b68
peccu <peccul@gmail.com>
2023-08-17 14:37:44 +0900

    merged buffer.el

--- conf.d/buffer.el
+++ conf.d/buffer.el
@@ -1,15 +1,21 @@
-;; バッファ画面外文字の切り詰め表示
-(setq truncate-lines nil)
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  ;; バッファ画面外文字の切り詰め表示
+  (setq truncate-lines nil)
 
-;; ウィンドウ縦分割時のバッファ画面外文字の切り詰め表示
-(setq truncate-partial-width-windows t)
+  ;; ウィンドウ縦分割時のバッファ画面外文字の切り詰め表示
+  (setq truncate-partial-width-windows t)
 
-;; 同一バッファ名にディレクトリ付与
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
-(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
-(setq uniquify-ignore-buffers-re "*[^*]+*")
+  ;; 同一バッファ名にディレクトリ付与
+  (require 'uniquify)
+  (setq uniquify-buffer-name-style 'forward)
+  (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+  (setq uniquify-ignore-buffers-re "*[^*]+*")
 
-;; バッファ一覧でソートする
-(define-key Buffer-menu-mode-map (kbd "S") 'Buffer-menu-sort)
-;; (insert "\n" (substitute-command-keys "\\{Buffer-menu-mode-map}"))
+  ;; バッファ一覧でソートする
+  (define-key Buffer-menu-mode-map (kbd "S") 'Buffer-menu-sort)
+  ;; (insert "\n" (substitute-command-keys "\\{Buffer-menu-mode-map}"))
+  )
