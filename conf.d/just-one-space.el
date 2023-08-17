6b50ff52ea66b6c1ce694efcfcd4ef68522bbc9c
peccu <peccul@gmail.com>
2023-08-17 15:45:45 +0900

    merged just-one-space.el

--- conf.d/just-one-space.el
+++ conf.d/just-one-space.el
@@ -1,4 +1,10 @@
-;; default
-;; (global-set-key (kbd "M-SPC") 'just-one-space)
-;; for spotlight/alfred/zazu
-(global-set-key (kbd "s-SPC") 'just-one-space)
+(when (or
+       ;; peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  ;; default
+  ;; (global-set-key (kbd "M-SPC") 'just-one-space)
+  ;; for spotlight/alfred/zazu
+  (global-set-key (kbd "s-SPC") 'just-one-space)
+  )
