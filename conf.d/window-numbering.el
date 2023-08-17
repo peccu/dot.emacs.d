f664307c679f8b538b618623af121e495b22518c
peccu <peccul@gmail.com>
2023-08-17 16:56:54 +0900

    merged window-numbering.el

--- conf.d/window-numbering.el
+++ conf.d/window-numbering.el
@@ -1,4 +1,10 @@
-(require 'window-numbering)
-;; (setq window-numbering-assign-func
-;;       (lambda () (when (equal (buffer-name) "*Calculator*") 9)))
-(window-numbering-mode t)
+(when (or
+       peccu-p
+       win-env-p
+       wsl-p
+       )
+  (require-with-install 'window-numbering)
+  ;; (setq window-numbering-assign-func
+  ;;       (lambda () (when (equal (buffer-name) "*Calculator*") 9)))
+  (window-numbering-mode t)
+  )
