d7b3f98649db6d515478c0a709b225ed161b78d7
peccu <peccul@gmail.com>
2023-08-17 14:16:17 +0900

    merged all-the-icons

--- conf.d/all-the-icons.el
+++ conf.d/all-the-icons.el
@@ -1,5 +1,11 @@
-(require 'all-the-icons)
+(when (or
+       ;; peccu-p
+       win-env-p
+       wsl-p
+       )
+(require-with-install 'all-the-icons)
 (setq inhibit-compacting-font-caches t)
 ;; M-x all-the-icons-install-fonts
 (when (require 'all-the-icons-dired nil t)
   (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
+)
