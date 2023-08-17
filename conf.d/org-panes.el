e89deeb7c84cc084b9cb0dfbfa2ec5c0bc990564
peccu <peccul@gmail.com>
2023-08-17 16:10:25 +0900

    merged org-panes.el

--- conf.d/org-panes.el
+++ conf.d/org-panes.el
@@ -1,6 +1,12 @@
-(eval-after-load "org"
-  '(progn
-     (require 'org-panes)
-     (setq org-panes-overview-depth 2)
-     (setq org-panes-contents-depth 3)
-     ))
+(when (or
+       ;; peccu-p
+       ;; win-env-p
+       ;; wsl-p
+       )
+  (eval-after-load "org"
+    '(progn
+       (require 'org-panes)
+       (setq org-panes-overview-depth 2)
+       (setq org-panes-contents-depth 3)
+       ))
+  )
