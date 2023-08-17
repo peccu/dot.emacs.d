4d88193aca7ca4bc86f28f5a00efec6ec341099f
peccu <peccul@gmail.com>
2023-08-17 15:46:19 +0900

    merged keyboard-macro.el

--- conf.d/keyboard-macro.el
+++ conf.d/keyboard-macro.el
@@ -1,10 +1,16 @@
-;; cf.http://d.hatena.ne.jp/rubikitch/20080413/1208029571
-(defun start-or-end-macro (arg)
-  (interactive "P")
-  (if defining-kbd-macro
-      (if arg
-          (end-kbd-macro arg)
-        (end-kbd-macro))
-    (start-kbd-macro arg)))
-(global-set-key (kbd "<f6>") 'call-last-kbd-macro)
-(global-set-key (kbd "S-<f6>") 'start-or-end-macro)
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  ;; cf.http://d.hatena.ne.jp/rubikitch/20080413/1208029571
+  (defun start-or-end-macro (arg)
+    (interactive "P")
+    (if defining-kbd-macro
+        (if arg
+            (end-kbd-macro arg)
+          (end-kbd-macro))
+      (start-kbd-macro arg)))
+  (global-set-key (kbd "<f6>") 'call-last-kbd-macro)
+  (global-set-key (kbd "S-<f6>") 'start-or-end-macro)
+  )
