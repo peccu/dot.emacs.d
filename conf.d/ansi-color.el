9733d27d5310f5fe7be6234a31190a84c5878c4c
peccu <peccul@gmail.com>
2023-08-17 14:18:05 +0900

    merged ansi-color and fix style

--- conf.d/ansi-color.el
+++ conf.d/ansi-color.el
@@ -1,5 +1,11 @@
-;; http://unix.stackexchange.com/questions/83381/visualizing-ansi-color-escape-codes-in-log-files-correctly-in-emacs
-(require 'ansi-color)
-(defun ansi-color-buffer()
-  (interactive)
-  (ansi-color-apply-on-region (point-min) (point-max)))
+(when (or
+       peccu-p
+       win-env-p
+       wsl-p
+       )
+  ;; http://unix.stackexchange.com/questions/83381/visualizing-ansi-color-escape-codes-in-log-files-correctly-in-emacs
+  (require 'ansi-color)
+  (defun ansi-color-buffer()
+    (interactive)
+    (ansi-color-apply-on-region (point-min) (point-max)))
+  )
