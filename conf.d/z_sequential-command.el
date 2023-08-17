95fe81180653069c82426efad531d3e6178bfb25
peccu <peccul@gmail.com>
2023-08-17 17:10:14 +0900

    merged z_sequential-command.el

--- conf.d/z_sequential-command.el
+++ conf.d/z_sequential-command.el
@@ -1,9 +1,18 @@
-(require 'sequential-command-config)
-(global-set-key (kbd "C-a") 'seq-home)
-(global-set-key (kbd "C-e") 'seq-end)
-(when (require 'org nil t)
-  (define-key org-mode-map (kbd "C-a") 'seq-home)
-  (define-key org-mode-map (kbd "C-e") 'seq-end))
-;; (define-key esc-map "u" 'seq-upcase-backward-word)
-;; (define-key esc-map "c" 'seq-capitalize-backward-word)
-;; (define-key esc-map "l" 'seq-downcase-backward-word)
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  (require-with-install 'sequential-command)
+  (require 'sequential-command-config)
+  (global-set-key (kbd "C-a") 'seq-home)
+  (global-set-key (kbd "C-e") 'seq-end)
+  (when (require 'org nil t)
+    (define-key org-mode-map (kbd "C-a") 'seq-home)
+    (define-key org-mode-map (kbd "C-e") 'seq-end))
+  ;; (define-key org-mode-map "\C-a" 'seq-home)
+  ;; (define-key org-mode-map "\C-e" 'seq-end)
+  ;; (define-key esc-map "u" 'seq-upcase-backward-word)
+  ;; (define-key esc-map "c" 'seq-capitalize-backward-word)
+  ;; (define-key esc-map "l" 'seq-downcase-backward-word)
+  )
