7b1b1029a7735567bbc35452591b6b50c5010f5e
peccu <peccul@gmail.com>
2023-08-17 16:49:56 +0900

    merged uniquify.el

--- conf.d/uniquify.el
+++ conf.d/uniquify.el
@@ -1,1 +1,8 @@
-(defalias 'uniquify-lines #'delete-duplicate-lines)
+(when (or
+       peccu-p
+       win-env-p
+       wsl-p
+       )
+  (when (fboundp 'uniquify-lines)
+    (defalias 'uniquify-lines #'delete-duplicate-lines))
+  )
