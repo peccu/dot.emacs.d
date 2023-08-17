ec07a29f171c8f6083ff5ba62bc720c327e754de
peccu <peccul@gmail.com>
2023-08-17 16:38:50 +0900

    merged tdd.el

--- conf.d/tdd.el
+++ conf.d/tdd.el
@@ -1,6 +1,12 @@
-(require 'tdd)
-;; tdd runs `recompile' when file saved.
-;; `recompile' exec `compile-command'
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (set (make-local-variable 'compile-command) "npm test")))
+(when (or
+       peccu-p
+       ;; win-env-p
+       ;; wsl-p
+       )
+  (require 'tdd)
+  ;; tdd runs `recompile' when file saved.
+  ;; `recompile' exec `compile-command'
+  (add-hook 'js2-mode-hook
+            (lambda ()
+              (set (make-local-variable 'compile-command) "npm test")))
+  )
