12a2bdab2412304cbe5577685e72356eea583387
peccu <peccul@gmail.com>
2023-08-17 16:33:39 +0900

    merged session.el

--- conf.d/session.el
+++ conf.d/session.el
@@ -1,15 +1,21 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; session
-;; ファイルの履歴や，カーソルの位置などを覚えておく
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'session)
-(add-hook 'after-init-hook 'session-initialize)
-;; ファイル保存時じゃなく，閉じたときに状態を保存する
-(setq session-undo-check -1)
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; session
+  ;; ファイルの履歴や，カーソルの位置などを覚えておく
+  ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (require-with-install 'session)
+  (add-hook 'after-init-hook 'session-initialize)
+  ;; ファイル保存時じゃなく，閉じたときに状態を保存する
+  (setq session-undo-check -1)
 
-;; ファイルの履歴の件数を30(default)からinfiniteへ．
-;; (session.elの設定ではないがここに記述する)
-(setq history-length t)
+  ;; ファイルの履歴の件数を30(default)からinfiniteへ．
+  ;; (session.elの設定ではないがここに記述する)
+  (setq history-length t)
 
-;; 保存先
-(setq session-save-file (concat user-emacs-directory ".session"))
+  ;; 保存先
+  (setq session-save-file (concat user-emacs-directory ".session"))
+  )
