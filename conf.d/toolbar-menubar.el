88076377e2f9fdaf6f75f5e99106c868c37094b1
peccu <peccul@gmail.com>
2023-08-17 16:39:44 +0900

    merged toolbar-menubar.el

--- conf.d/toolbar-menubar.el
+++ conf.d/toolbar-menubar.el
@@ -1,3 +1,9 @@
-(tool-bar-mode 0)
-(menu-bar-mode 0)
-(scroll-bar-mode 0)
+(when (or
+       peccu-p
+       win-env-p
+       wsl-p
+       )
+  (tool-bar-mode 0)
+  (menu-bar-mode 0)
+  (scroll-bar-mode 0)
+  )
