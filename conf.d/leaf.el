28820a89510bcbef864c0ebb48a5b7e3ad90b0d3
peccu <peccul@gmail.com>
2023-08-17 15:49:04 +0900

    merged leaf.el

--- conf.d/leaf.el
+++ conf.d/leaf.el
@@ -1,3 +1,28 @@
-(unless (package-installed-p 'leaf)
-  (package-refresh-contents)
-  (package-install 'leaf))
+;; https://github.com/conao3/leaf.el#install
+(when (or
+       ;; peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+;; (eval-and-compile
+;;   (customize-set-variable
+;;    'package-archives '(("org" . "https://orgmode.org/elpa/")
+;;                        ("melpa" . "https://melpa.org/packages/")
+;;                        ("gnu" . "https://elpa.gnu.org/packages/")))
+;;   (package-initialize)
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
+
+  ;; (leaf leaf-keywords
+  ;;       :ensure t
+  ;;       :init
+  ;;       ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
+  ;;       (leaf hydra :ensure t)
+  ;;       (leaf el-get :ensure t)
+  ;;       (leaf blackout :ensure t)
+
+  ;;       :config
+  ;;       ;; initialize leaf-keywords.el
+  ;;       (leaf-keywords-init))
+  )
