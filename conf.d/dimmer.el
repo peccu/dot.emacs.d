eb592be7888877cd32445dc028c5d1d85c3ed292
peccu <peccul@gmail.com>
2023-08-17 14:52:02 +0900

    merged dimmer.el

--- conf.d/dimmer.el
+++ conf.d/dimmer.el
@@ -1,3 +1,9 @@
-(require 'dimmer)
-(setq dimmer-fraction 0.2)
-(dimmer-mode)
+(when (or
+       ;; peccu-p
+       ;; win-env-p
+       ;; wsl-p
+       )
+  (require 'dimmer)
+  (setq dimmer-fraction 0.2)
+  (dimmer-mode)
+  )
