11961de1e0659b73e07a1910a68bb6d5f96cb40b
peccu <peccul@gmail.com>
2023-08-17 14:55:28 +0900

    merged docker.el

--- conf.d/docker.el
+++ conf.d/docker.el
@@ -1,6 +1,12 @@
-(require 'docker)
-(require 'dockerfile-mode)
-(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
-(require 'docker-compose-mode)
-(require 'docker-tramp-compat)
-(set-variable 'docker-tramp-use-names t)
+(when (or
+       ;; peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  (require-with-install 'docker)
+  (require-with-install 'dockerfile-mode)
+  (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+  (require-with-install 'docker-compose-mode)
+  ;; (require-with-install 'docker-tramp-compat)
+  ;; (set-variable 'docker-tramp-use-names t)
+  )
