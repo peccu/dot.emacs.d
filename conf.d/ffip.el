d8516aa2d7391f4f808b49232861f8c11b14bf7e
peccu <peccul@gmail.com>
2023-08-17 15:07:13 +0900

    merged ffip.el

--- conf.d/ffip.el
+++ conf.d/ffip.el
@@ -1,3 +1,9 @@
-(require 'find-file-in-project)
-;; brew install fd
-(setq ffip-use-rust-fd t)
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  (require-with-install 'find-file-in-project)
+  ;; brew install fd
+  (setq ffip-use-rust-fd t)
+  )
