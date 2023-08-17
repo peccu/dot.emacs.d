2438db1dd05438467d8a233e0e23b76f904d39ff
peccu <peccul@gmail.com>
2023-08-17 14:14:43 +0900

    fix istyle

--- conf.d/0tramp.el
+++ conf.d/0tramp.el
@@ -3,21 +3,21 @@
        win-env-p
        wsl-p
        )
-;; (add-to-list 'load-path "~/.emacs.d//git/tramp/lisp")
-(require 'tramp)
+  ;; (add-to-list 'load-path "~/.emacs.d//git/tramp/lisp")
+  (require 'tramp)
 
-(add-to-list 'tramp-remote-process-environment
-             (format "DISPLAY=%s" (getenv "DISPLAY")))
+  (add-to-list 'tramp-remote-process-environment
+               (format "DISPLAY=%s" (getenv "DISPLAY")))
 
-(add-to-list 'tramp-remote-process-environment
-             "HISTFILE=/dev/null")
-;; (setq tramp-remote-process-environment
-;;       `("TMOUT=0" "LC_CTYPE=''"
-;;         ,(format "TERM=%s" tramp-terminal-type)
-;;         ,(format "INSIDE_EMACS='%s,tramp:%s'" emacs-version tramp-version)
-;;         "CDPATH=" "HISTORY=" "MAIL=" "MAILCHECK=" "MAILPATH=" "PAGER=cat"
-;;         "autocorrect=" "correct="))
+  (add-to-list 'tramp-remote-process-environment
+               "HISTFILE=/dev/null")
+  ;; (setq tramp-remote-process-environment
+  ;;       `("TMOUT=0" "LC_CTYPE=''"
+  ;;         ,(format "TERM=%s" tramp-terminal-type)
+  ;;         ,(format "INSIDE_EMACS='%s,tramp:%s'" emacs-version tramp-version)
+  ;;         "CDPATH=" "HISTORY=" "MAIL=" "MAILCHECK=" "MAILPATH=" "PAGER=cat"
+  ;;         "autocorrect=" "correct="))
 
-;; (defadvice tramp-handle-vc-registered (around tramp-handle-vc-registered-around activate)
-;;   (let ((vc-handled-backends '(Git))) ad-do-it))
-)
+  ;; (defadvice tramp-handle-vc-registered (around tramp-handle-vc-registered-around activate)
+  ;;   (let ((vc-handled-backends '(Git))) ad-do-it))
+  )
