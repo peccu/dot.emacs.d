48014843927a8ecba966e9c35c0d5ec4dfa5f1a7
peccu <peccul@gmail.com>
2023-08-17 16:44:30 +0900

    merged trim-buffer.el

--- conf.d/trim-buffer.el
+++ conf.d/trim-buffer.el
@@ -1,22 +1,28 @@
-;; cf.http://www.chibutsu.org/faf/WebWiki/emacs/kwatch-mule.html#sec49
-;; 余分な空白を削除する
-(defun trim-buffer ()
-  "Delete excess white space."
-  (interactive)
-  (save-excursion
-    ;; 行末の空白を削除する
-    (goto-char (point-min))
-    (while (re-search-forward "[ \t]+$" nil t)
-      (replace-match "" nil nil))
-    ;; ファイルの終わりにある空白行を削除する
-    (goto-char (point-max))
-    (delete-blank-lines)
-    ;; タブを空白に変換する
-    (mark-whole-buffer)
-    (untabify (region-beginning) (region-end))
-    ))
+(when (or
+       peccu-p
+       win-env-p
+       wsl-p
+       )
+  ;; cf.http://www.chibutsu.org/faf/WebWiki/emacs/kwatch-mule.html#sec49
+  ;; 余分な空白を削除する
+  (defun trim-buffer ()
+    "Delete excess white space."
+    (interactive)
+    (save-excursion
+      ;; 行末の空白を削除する
+      (goto-char (point-min))
+      (while (re-search-forward "[ \t]+$" nil t)
+        (replace-match "" nil nil))
+      ;; ファイルの終わりにある空白行を削除する
+      (goto-char (point-max))
+      (delete-blank-lines)
+      ;; タブを空白に変換する
+      (mark-whole-buffer)
+      (untabify (region-beginning) (region-end))
+      ))
 
-;; ファイルをセーブしたときに自動的に実行させる
-;; (add-hook 'write-file-hooks
-;;           '(lambda ()
-;;              (trim-buffer)))
+  ;; ファイルをセーブしたときに自動的に実行させる
+  ;; (add-hook 'write-file-hooks
+  ;;           '(lambda ()
+  ;;              (trim-buffer)))
+  )
