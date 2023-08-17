d449f289e1213eef00d237fb37a5f3ad5721c552
peccu <peccul@gmail.com>
2023-08-17 16:42:12 +0900

    merged trans-regions.el

--- conf.d/trans-regions.el
+++ conf.d/trans-regions.el
@@ -1,2 +1,8 @@
-(require 'trans-regions)
-(define-key global-map (kbd "C-c t") 'trans-regions)
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  (require 'trans-regions)
+  (define-key global-map (kbd "C-c t") 'trans-regions)
+  )
