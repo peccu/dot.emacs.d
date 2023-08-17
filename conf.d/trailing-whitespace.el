2e541691309e6e122485c6ff63f902ceb28ceb80
peccu <peccul@gmail.com>
2023-08-17 16:40:42 +0900

    merged trailing-whitespace.el

--- conf.d/trailing-whitespace.el
+++ conf.d/trailing-whitespace.el
@@ -1,4 +1,10 @@
-(when (boundp 'show-trailing-whitespace)
-  (setq-default show-trailing-whitespace t))
-(set-face-background 'trailing-whitespace "purple4")
-;; show space and tabs -> highlight-chars.el
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  (when (boundp 'show-trailing-whitespace)
+    (setq-default show-trailing-whitespace t))
+  (set-face-background 'trailing-whitespace "purple4")
+  ;; show space and tabs -> highlight-chars.el
+  )
