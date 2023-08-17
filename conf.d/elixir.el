784da107c1b2fdb61923f39ea1e0888a09674ec2
peccu <peccul@gmail.com>
2023-08-17 15:01:24 +0900

    merged elixir.el

--- conf.d/elixir.el
+++ conf.d/elixir.el
@@ -1,7 +1,13 @@
-(require 'elixir-mode)
-(require 'alchemist)
-(add-hook 'elixir-mode-hook 'ac-alchemist-setup)
-(add-hook 'elixir-mode-hook
-          (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
-(add-to-list 'auto-mode-alist '("\\.leex\\'" . elixir-mode))
-(add-hook 'elixir-mode-hook 'auto-highlight-symbol-mode)
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  (require-with-install 'elixir-mode)
+  (require-with-install 'alchemist)
+  (add-hook 'elixir-mode-hook 'ac-alchemist-setup)
+  (add-hook 'elixir-mode-hook
+            (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
+  (add-to-list 'auto-mode-alist '("\\.leex\\'" . elixir-mode))
+  (add-hook 'elixir-mode-hook 'auto-highlight-symbol-mode)
+  )
