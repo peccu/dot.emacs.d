026f235a095eae9215c09651aa73aef3d88ddce4
peccu <peccul@gmail.com>
2023-08-17 16:22:12 +0900

    merged quickrun.el

--- conf.d/quickrun.el
+++ conf.d/quickrun.el
@@ -1,5 +1,11 @@
-(require 'quickrun)
-;; quickrun実行後に結果バッファにフォーカスを移さない
-(setq quickrun-focus-p nil)
-(global-set-key (kbd "M-g M-q") 'fill-paragraph)
-(global-set-key (kbd "M-q") 'quickrun)
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  (require-with-install 'quickrun)
+  ;; quickrun実行後に結果バッファにフォーカスを移さない
+  (setq quickrun-focus-p nil)
+  (global-set-key (kbd "M-g M-q") 'fill-paragraph)
+  (global-set-key (kbd "M-q") 'quickrun)
+  )
