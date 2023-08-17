18b432a4354d96e3136029018ae883d86c84fff4
peccu <peccul@gmail.com>
2023-08-17 16:28:18 +0900

    merged replace-all.el

--- conf.d/replace-all.el
+++ conf.d/replace-all.el
@@ -1,15 +1,21 @@
-;; Emacs Lispテクニックバイブル p.111 replace-regexp-in-lisp より
-(defun replace-all (regexp to-string)
-  "Emacs Lispテクニックバイブル p.111 replace-regexp-in-lisp より"
-  (save-excursion
-    (goto-char (point-min))
-    (replace-all-from-here regexp to-string)))
+(when (or
+       peccu-p
+       win-env-p
+       wsl-p
+       )
+  ;; Emacs Lispテクニックバイブル p.111 replace-regexp-in-lisp より
+  (defun replace-all (regexp to-string)
+    "Emacs Lispテクニックバイブル p.111 replace-regexp-in-lisp より"
+    (save-excursion
+      (goto-char (point-min))
+      (replace-all-from-here regexp to-string)))
 
-(defun replace-all-from-here (regexp to-string)
-  (save-excursion
-    (while (re-search-forward regexp nil t)
-      (replace-match to-string))))
+  (defun replace-all-from-here (regexp to-string)
+    (save-excursion
+      (while (re-search-forward regexp nil t)
+        (replace-match to-string))))
 
-(defun query-replace-all (from-string to-string)
-  (save-excursion
-    (query-replace from-string to-string nil (point-min) (point-max))))
+  (defun query-replace-all (from-string to-string)
+    (save-excursion
+      (query-replace from-string to-string nil (point-min) (point-max))))
+  )
