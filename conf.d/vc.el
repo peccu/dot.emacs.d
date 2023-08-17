a3e1447c3bfd1cd12fd48723ae590c7172be2f67
peccu <peccul@gmail.com>
2023-08-17 16:51:07 +0900

    merged vc.el

--- conf.d/vc.el
+++ conf.d/vc.el
@@ -1,4 +1,10 @@
-;; remove hook
-;; http://stackoverflow.com/questions/5748814/how-does-one-disable-vc-git-in-emacs
-(eval-after-load "vc" '(remove-hook 'find-file-hooks 'vc-find-file-hook))
-(setq vc-handled-backends nil)
+(when (or
+       ;; peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  ;; remove hook
+  ;; http://stackoverflow.com/questions/5748814/how-does-one-disable-vc-git-in-emacs
+  (eval-after-load "vc" '(remove-hook 'find-file-hooks 'vc-find-file-hook))
+  (setq vc-handled-backends nil)
+  )
