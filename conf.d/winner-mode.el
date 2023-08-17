570af6b17888e61f9c136f3253dade28d2b7dfe1
peccu <peccul@gmail.com>
2023-08-17 17:05:06 +0900

    merged winner-mode.el

--- conf.d/winner-mode.el
+++ conf.d/winner-mode.el
@@ -1,3 +1,9 @@
-;; http://www.emacswiki.org/emacs/WinnerMode
-(when (fboundp 'winner-mode)
-  (winner-mode 1))
+(when (or
+       peccu-p
+       win-env-p
+       wsl-p
+       )
+  ;; http://www.emacswiki.org/emacs/WinnerMode
+  (when (fboundp 'winner-mode)
+    (winner-mode 1))
+  )
