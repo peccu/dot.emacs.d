79879249083c7da7fb0111ea71010207544286fb
peccu <peccul@gmail.com>
2023-08-17 16:21:22 +0900

    merged ffap.el

--- conf.d/ffap.el
+++ conf.d/ffap.el
@@ -1,3 +1,9 @@
-(ffap-bindings)
-;; swap to default find-alternate-file
-(global-set-key (kbd "C-x C-v") 'find-alternate-file)
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  (ffap-bindings)
+  ;; swap to default find-alternate-file
+  (global-set-key (kbd "C-x C-v") 'find-alternate-file)
+  )
