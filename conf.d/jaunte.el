44c2d19d30ca68bde8b6597c08532512bc9cd755
peccu <peccul@gmail.com>
2023-08-17 15:41:09 +0900

    merged jaunte.el

--- conf.d/jaunte.el
+++ conf.d/jaunte.el
@@ -1,4 +1,10 @@
-;; jaunte
-;; (require 'jaunte-plus)
-(require 'jaunte)
-(global-set-key (kbd "M-s") 'jaunte)
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  ;; jaunte
+  ;; (require 'jaunte-plus)
+  (require 'jaunte)
+  (global-set-key (kbd "M-s") 'jaunte)
+  )
