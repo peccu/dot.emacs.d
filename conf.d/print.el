ef6432c16581db3a2e4ee6b31f40635818148027
peccu <peccul@gmail.com>
2023-08-17 16:17:50 +0900

    merged print.el

--- conf.d/print.el
+++ conf.d/print.el
@@ -1,9 +1,15 @@
-(setq ;; ps-print-color-p t
-      ;; ps-lpr-command "gswin32c.exe"
-      ;; ps-multibyte-buffer 'non-latin-printer
-      ;; ps-lpr-switches '("-sDEVICE=mswinpr2" "-dNOPAUSE" "-dBATCH" "-dWINKANJI")
-      ;; printer-name nil
-      ;; ps-printer-name nil
-      ;; ps-printer-name-option nil
-      ps-print-header nil          ; ヘッダの非表示
-      )
+(when (or
+       ;; peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  (setq ;; ps-print-color-p t
+   ;; ps-lpr-command "gswin32c.exe"
+   ;; ps-multibyte-buffer 'non-latin-printer
+   ;; ps-lpr-switches '("-sDEVICE=mswinpr2" "-dNOPAUSE" "-dBATCH" "-dWINKANJI")
+   ;; printer-name nil
+   ;; ps-printer-name nil
+   ;; ps-printer-name-option nil
+   ps-print-header nil          ; ヘッダの非表示
+   )
+  )
