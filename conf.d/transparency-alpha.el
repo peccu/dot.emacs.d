cedf68862451bd6c3dac37a5fd2da36d8b45ad76
peccu <peccul@gmail.com>
2023-08-17 16:44:04 +0900

    merged transparency-alpha.el

--- conf.d/transparency-alpha.el
+++ conf.d/transparency-alpha.el
@@ -1,14 +1,21 @@
-(defun toggle-transparency ()
-  (interactive)
-  (let ((alpha (frame-parameter nil 'alpha)))
-    (set-frame-parameter
-     nil 'alpha
-     (if (eql (cond ((numberp alpha) alpha)
-                    ((numberp (cdr alpha)) (cdr alpha))
-                    ;; Also handle undocumented (<active> <inactive>) form.
-                    ((numberp (cadr alpha)) (cadr alpha)))
-              100)
-         '(65 . 50)                     ;active, inactive
-       '(100 . 100)))))
-(toggle-transparency)
-;; (global-set-key (kbd "C-c t") 'toggle-transparency)
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  (defun toggle-transparency ()
+    (interactive)
+    (let ((alpha (frame-parameter nil 'alpha)))
+      (set-frame-parameter
+       nil 'alpha
+       (if (eql (cond ((numberp alpha) alpha)
+                      ((numberp (cdr alpha)) (cdr alpha))
+                      ;; Also handle undocumented (<active> <inactive>) form.
+                      ((numberp (cadr alpha)) (cadr alpha)))
+                100)
+           '(65 . 50)                     ;active, inactive
+         '(100 . 100)))))
+  (toggle-transparency)
+  ;; (global-set-key (kbd "C-c t") 'toggle-transparency)
+  ;; -> C-c t is bound to trans-regions
+  )
