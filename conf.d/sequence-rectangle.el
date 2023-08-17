6fcae2b8f6ad0788a19dbc9e01d289a44692114f
peccu <peccul@gmail.com>
2023-08-17 16:32:00 +0900

    merged sequence-rectangle.el

--- conf.d/sequence-rectangle.el
+++ conf.d/sequence-rectangle.el
@@ -1,2 +1,8 @@
-(require 'my-sequence-rectangle)
-(define-key ctl-x-map "rN" 'my-sequence-rectangle)
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  (require 'my-sequence-rectangle)
+  (define-key ctl-x-map "rN" 'my-sequence-rectangle)
+  )
