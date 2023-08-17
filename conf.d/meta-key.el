5fe1c9e5c6c05b98fc3c94269583981e5f696c37
peccu <peccul@gmail.com>
2023-08-17 15:57:05 +0900

    merged meta-key.el

--- conf.d/meta-key.el
+++ conf.d/meta-key.el
@@ -1,4 +1,10 @@
-;; Cocoa EmacsでメタキーをCommandキーにする
-(when darwin-p
-  (setq ns-command-modifier 'meta)
-  (setq ns-alternate-modifier 'super))
+(when (or
+       peccu-p
+       ;; win-env-p
+       ;; wsl-p
+       )
+  ;; Cocoa EmacsでメタキーをCommandキーにする
+  (when darwin-p
+    (setq ns-command-modifier 'meta)
+    (setq ns-alternate-modifier 'super))
+  )
