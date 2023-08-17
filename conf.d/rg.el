349b3e686cef8e2cf09a36a38c0df7a3c8f51123
peccu <peccul@gmail.com>
2023-08-17 16:29:22 +0900

    merged rg.el

--- conf.d/rg.el
+++ conf.d/rg.el
@@ -1,6 +1,14 @@
-;; for coreos
-(setq rg-executable "/opt/bin/rg")
-(defun rg-command ()
-  "Command string for invoking rg."
-  (concat (rg-executable)
-          " --colors match:fg:red -n"))
+(when (or
+       peccu-p
+       win-env-p
+       wsl-p
+       )
+  (when win-env-p
+    ;; for coreos
+    (setq rg-executable "/opt/bin/rg")
+    )
+  (defun rg-command ()
+    "Command string for invoking rg."
+    (concat (rg-executable)
+            " --colors match:fg:red -n"))
+  )
