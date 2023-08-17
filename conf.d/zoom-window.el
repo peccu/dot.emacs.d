b82f2cc16a55fa99f74a868d98ebd0a57c81b3fe
peccu <peccul@gmail.com>
2023-08-17 17:17:42 +0900

    merged zoom-window.el

--- conf.d/zoom-window.el
+++ conf.d/zoom-window.el
@@ -1,4 +1,10 @@
-(require 'zoom-window)
-;; overwrite windows.el
-(eval-after-load 'windows
-  (define-key win:switch-map (kbd "SPC") 'zoom-window-zoom))
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  (require-with-install 'zoom-window)
+  ;; overwrite windows.el
+  (eval-after-load 'windows
+    (define-key win:switch-map (kbd "SPC") 'zoom-window-zoom))
+  )
