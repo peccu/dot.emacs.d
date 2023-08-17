d45f212e2773740af45c3d27a37abf5059b0f9dd
peccu <peccul@gmail.com>
2023-08-17 16:55:50 +0900

    merged windmove.el

--- conf.d/windmove.el
+++ conf.d/windmove.el
@@ -1,10 +1,16 @@
-;; ;; move splited frame
-;; ;; Shift-(left|right|up|down)
-(windmove-default-keybindings)
-(setq windmove-wrap-around t)
-(defun counter-other-window ()
-  (interactive)
-  (other-window -1))
-(global-set-key (kbd "C-;") 'other-window)
-;; (global-set-key (kbd "C-:") 'counter-other-window)
-(global-set-key (kbd "C-M-;") 'counter-other-window)
+(when (or
+       peccu-p
+       win-env-p
+       wsl-p
+       )
+  ;; ;; move splited frame
+  ;; ;; Shift-(left|right|up|down)
+  (windmove-default-keybindings)
+  (setq windmove-wrap-around t)
+  (defun counter-other-window ()
+    (interactive)
+    (other-window -1))
+  (global-set-key (kbd "C-;") 'other-window)
+  ;; (global-set-key (kbd "C-:") 'counter-other-window)
+  (global-set-key (kbd "C-M-;") 'counter-other-window)
+  )
