2edf94bcecb971c85d6854f4b3df633f2bc79e64
peccu <peccul@gmail.com>
2023-08-17 16:07:25 +0900

    merged nxml.el

--- conf.d/nxml.el
+++ conf.d/nxml.el
@@ -1,7 +1,13 @@
-(add-to-list 'auto-mode-alist '("\\.tpl\\'" . nxml-mode))
-(add-hook 'nxml-mode-hook
-          '(lambda ()
-             (setq nxml-child-indent 2
-                   nxml-attribute-indent 2)
-             (define-key nxml-mode-map (kbd "M-}") 'forward-paragraph)
-             (define-key nxml-mode-map (kbd "M-{") 'backward-paragraph)))
+(when (or
+       ;; peccu-p
+       ;; win-env-p
+       ;; wsl-p
+       )
+  (add-to-list 'auto-mode-alist '("\\.tpl\\'" . nxml-mode))
+  (add-hook 'nxml-mode-hook
+            '(lambda ()
+               (setq nxml-child-indent 2
+                     nxml-attribute-indent 2)
+               (define-key nxml-mode-map (kbd "M-}") 'forward-paragraph)
+               (define-key nxml-mode-map (kbd "M-{") 'backward-paragraph)))
+  )
