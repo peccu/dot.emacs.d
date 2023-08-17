c296fc05eee05cf17d0b4add18057e628e612fc1
peccu <peccul@gmail.com>
2023-08-17 16:50:33 +0900

    merged url-encode.el

--- conf.d/url-encode.el
+++ conf.d/url-encode.el
@@ -1,28 +1,34 @@
-;; (require 'url-util)
-;; (url-hexify-string "やっほー")
-;; (url-encode-url "やっほー")
+(when (or
+       peccu-p
+       win-env-p
+       wsl-p
+       )
+  ;; (require 'url-util)
+  ;; (url-hexify-string "やっほー")
+  ;; (url-encode-url "やっほー")
 
-;; http://hins11.jugem.jp/?eid=34
-(defun my-url-encode-string (str &optional sys)
-  (let ((sys (or sys 'utf-8)))
-    (url-hexify-string (encode-coding-string str sys))))
+  ;; http://hins11.jugem.jp/?eid=34
+  (defun my-url-encode-string (str &optional sys)
+    (let ((sys (or sys 'utf-8)))
+      (url-hexify-string (encode-coding-string str sys))))
 
-(defun my-url-decode-string (str &optional sys)
-  (let ((sys (or sys 'utf-8)))
-    (decode-coding-string (url-unhex-string str) sys)))
+  (defun my-url-decode-string (str &optional sys)
+    (let ((sys (or sys 'utf-8)))
+      (decode-coding-string (url-unhex-string str) sys)))
 
-(defun my-url-decode-region (beg end)
-  (interactive "r")
-  (let ((pos beg)
-        (str (buffer-substring beg end)))
-    (goto-char beg)
-    (delete-region beg end)
-    (insert (my-url-decode-string str 'utf-8))))
+  (defun my-url-decode-region (beg end)
+    (interactive "r")
+    (let ((pos beg)
+          (str (buffer-substring beg end)))
+      (goto-char beg)
+      (delete-region beg end)
+      (insert (my-url-decode-string str 'utf-8))))
 
-(defun my-url-encode-region (beg end)
-  (interactive "r")
-  (let ((pos beg)
-        (str (buffer-substring beg end)))
-    (goto-char beg)
-    (delete-region beg end)
-    (insert (my-url-encode-string str 'utf-8))))
+  (defun my-url-encode-region (beg end)
+    (interactive "r")
+    (let ((pos beg)
+          (str (buffer-substring beg end)))
+      (goto-char beg)
+      (delete-region beg end)
+      (insert (my-url-encode-string str 'utf-8))))
+  )
