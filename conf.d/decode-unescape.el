d2726d2fd5417745e5f1abd31650f362bda3ca41
peccu <peccul@gmail.com>
2023-08-17 14:51:17 +0900

    merged decode-unescape.el

--- conf.d/decode-unescape.el
+++ conf.d/decode-unescape.el
@@ -1,29 +1,35 @@
-;; https://emacs.stackexchange.com/a/3142
-(eval-after-load 'web-mode
-  '(progn
-     (defalias 'decode-xml-escape 'web-mode-dom-entities-replace)
-     (defalias 'unescape-xml-entities 'web-mode-dom-entities-replace)
-     ))
+(when (or
+       ;; peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  ;; https://emacs.stackexchange.com/a/3142
+  (eval-after-load 'web-mode
+    '(progn
+       (defalias 'decode-xml-escape 'web-mode-dom-entities-replace)
+       (defalias 'unescape-xml-entities 'web-mode-dom-entities-replace)
+       ))
 
-;; https://emacs.stackexchange.com/a/3141
-(defalias 'decode-entities
-  (if (fboundp 'libxml-parse-html-region) 'decode-entities/libxml 'decode-entities/xml.el))
+  ;; https://emacs.stackexchange.com/a/3141
+  (defalias 'decode-entities
+    (if (fboundp 'libxml-parse-html-region) 'decode-entities/libxml 'decode-entities/xml.el))
 
-(defun decode-entities/xml.el (html)
-  (with-temp-buffer
-    (save-excursion (insert html))
-    (xml-parse-string)))
+  (defun decode-entities/xml.el (html)
+    (with-temp-buffer
+      (save-excursion (insert html))
+      (xml-parse-string)))
 
-(defun decode-entities/libxml (html)
-  (with-temp-buffer
-    (insert html)
-    (let ((document
-           (libxml-parse-html-region (point-min) (point-max))))
-      (pcase document
-        (`(html nil
-                (body nil
-                      (p nil
-                         ,(and (pred stringp)
-                               content))))
-         content)
-        (_ (error "Unexpected parse result: %S" document))))))
+  (defun decode-entities/libxml (html)
+    (with-temp-buffer
+      (insert html)
+      (let ((document
+             (libxml-parse-html-region (point-min) (point-max))))
+        (pcase document
+          (`(html nil
+                  (body nil
+                        (p nil
+                           ,(and (pred stringp)
+                                 content))))
+           content)
+          (_ (error "Unexpected parse result: %S" document))))))
+  )
