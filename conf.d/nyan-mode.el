b7337ca4192b10f39ec8bed11f99e49ede1497c9
peccu <peccul@gmail.com>
2023-08-17 16:08:16 +0900

    merged nyan-mode.el

--- conf.d/nyan-mode.el
+++ conf.d/nyan-mode.el
@@ -1,6 +1,12 @@
-(require 'nyan-mode)
-(setq nyan-animation-frame-interval 0.1)
-(setq nyan-bar-length 6)
-(nyan-mode)
-(nyan-start-animation)
-;; (nyan-stop-animation)
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  (require-with-install 'nyan-mode)
+  (setq nyan-animation-frame-interval 0.1)
+  (setq nyan-bar-length 6)
+  (nyan-mode)
+  (nyan-start-animation)
+  ;; (nyan-stop-animation)
+  )
