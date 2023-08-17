dda64cbef7c1040fcf17f9ea1b85c1eab3e04619
peccu <peccul@gmail.com>
2023-08-17 16:35:45 +0900

    merged sourcetrail.el

--- conf.d/sourcetrail.el
+++ conf.d/sourcetrail.el
@@ -1,3 +1,9 @@
-(require 'sourcetrail)
-;; M-x sourcetrail-mode for renderer
-;; M-x sourcetrail-send-loation to send location to SourceTrail
+(when (or
+       ;; peccu-p
+       ;; win-env-p
+       ;; wsl-p
+       )
+  (require 'sourcetrail)
+  ;; M-x sourcetrail-mode for renderer
+  ;; M-x sourcetrail-send-loation to send location to SourceTrail
+  )
