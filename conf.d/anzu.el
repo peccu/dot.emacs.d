a037c1c488b74bdd13ed2e0df5c72b8258b10aef
peccu <peccul@gmail.com>
2023-08-17 14:24:25 +0900

    merge anzu

--- conf.d/anzu.el
+++ conf.d/anzu.el
@@ -1,21 +1,27 @@
-(require 'anzu)
-(global-anzu-mode +1)
+(when (or
+       peccu-p
+       win-env-p
+       wsl-p
+       )
+  (require-with-install 'anzu)
+  (global-anzu-mode +1)
 
-(set-face-attribute 'anzu-mode-line nil
-                    :foreground "red" :weight 'bold)
-;;                     :foreground "yellow" :weight 'bold)
-;; (defun my/anzu-update-func (here total)
-;;   (propertize (format "<%d/%d>" here total)
-;;               'face '((:foreground "yellow" :weight bold))))
-;; (setq anzu-mode-line-update-function 'my/update-func)
+  (set-face-attribute 'anzu-mode-line nil
+                      :foreground "red" :weight 'bold)
+  ;;                     :foreground "yellow" :weight 'bold)
+  ;; (defun my/anzu-update-func (here total)
+  ;;   (propertize (format "<%d/%d>" here total)
+  ;;               'face '((:foreground "yellow" :weight bold))))
+  ;; (setq anzu-mode-line-update-function 'my/update-func)
 
-(custom-set-variables
- '(anzu-mode-lighter "")
- '(anzu-deactivate-region t)
- '(anzu-search-threshold 1000)
- '(anzu-replace-to-string-separator " => "))
+  (custom-set-variables
+   '(anzu-mode-lighter "")
+   '(anzu-deactivate-region t)
+   '(anzu-search-threshold 1000)
+   '(anzu-replace-to-string-separator " => "))
 
-;; replace query replace
-(global-set-key (kbd "M-%") 'anzu-query-replace)
-(global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp)
-(global-set-key (kbd "C-x %") 'anzu-replace-at-cursor-thing)
+  ;; replace query replace
+  (global-set-key (kbd "M-%") 'anzu-query-replace)
+  (global-set-key (kbd "C-M-%") 'anzu-query-replace-regexp)
+  (global-set-key (kbd "C-x %") 'anzu-replace-at-cursor-thing)
+  )
