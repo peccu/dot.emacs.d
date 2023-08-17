de1b06d97e180febece6a63abbbd4d3d02303f44
peccu <peccul@gmail.com>
2023-08-17 14:42:59 +0900

    merged charset.el

--- conf.d/charset.el
+++ conf.d/charset.el
@@ -1,2 +1,9 @@
-(set-language-environment "UTF-8")
-(setq-default buffer-file-coding-system 'utf-8-unix)
+(when (or
+       peccu-p
+       win-env-p
+       wsl-p
+       )
+  ;; http://ergoemacs.org/emacs/emacs_encoding_decoding_faq.html
+  (set-language-environment "UTF-8")
+  (setq-default buffer-file-coding-system 'utf-8-unix)
+  )
