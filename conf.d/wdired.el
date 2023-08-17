f5ab1f130058097e3d91100d14b7328a77002c5e
peccu <peccul@gmail.com>
2023-08-17 16:51:55 +0900

    merged wdired.el

--- conf.d/wdired.el
+++ conf.d/wdired.el
@@ -1,2 +1,8 @@
-(require 'wdired)
-(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
+(when (or
+       peccu-p
+       win-env-p
+       wsl-p
+       )
+  (require-with-install 'wdired)
+  (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
+  )
