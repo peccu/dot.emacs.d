c14b2565cdd5a9a6f87029c12c5d9f802d326fcb
peccu <peccul@gmail.com>
2023-08-17 16:16:20 +0900

    merged popup-color-at-point.el

--- conf.d/popup-color-at-point.el
+++ conf.d/popup-color-at-point.el
@@ -1,2 +1,8 @@
-(load "~/.emacs.d/lisp/popup-color-at-point.el")
-(global-set-key (kbd "C-c c") 'popup-color-at-point)
+(when (or
+       ;; peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  (load "~/.emacs.d/lisp/popup-color-at-point.el")
+  (global-set-key (kbd "C-c c") 'popup-color-at-point)
+  )
