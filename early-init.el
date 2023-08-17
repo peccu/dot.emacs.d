d7d76727288fda35ad06fcb9d42ca7d2d0753800
peccu <peccul@gmail.com>
2023-08-17 11:22:31 +0900

    add early init

---
+++ early-init.el
@@ -0,0 +1,24 @@
+;; -*- mode: emacs-lisp; coding: utf-8 -*-
+;; after 27, this file should be loaded before GUI setup
+
+;; env detection
+(load-file (concat user-emacs-directory "lisp/version-detect.el"))
+(load-file (concat user-emacs-directory "env-detect.el"))
+
+(if wsl-p
+    (setq package-user-dir "/aws/.emacs.d/elpa-wsl")
+  (setq package-user-dir (concat user-emacs-directory "elpa-" emacs-version "-" emacs-os)))
+
+;; https://uwabami.github.io/cc-env/Emacs.html
+(push '(vertical-scroll-bars . nil) default-frame-alist)
+(push '(menu-bar-lines       . nil) default-frame-alist)
+(push '(tool-bar-lines       . nil) default-frame-alist)
+(push '(scroll-bar-mode      . nil) default-frame-alist)
+(push '(blink-cursor-mode    . nil) default-frame-alist)
+(push '(column-number-mode   . nil) default-frame-alist)
+;; (setq load-prefer-newer noninteractive)
+;; (setq frame-inhibit-implied-resize t)
+;; (setq site-run-file nil)
+;; (setq window-divider-default-right-width 3)
+;; (setq package-enable-at-startup nil)
+(provide 'early-init)
