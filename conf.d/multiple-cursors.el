0c0d804afb286a17dd60abb76c8f73a29865a738
peccu <peccul@gmail.com>
2023-08-17 16:05:53 +0900

    merged multiple-cursors.el

--- conf.d/multiple-cursors.el
+++ conf.d/multiple-cursors.el
@@ -1,10 +1,16 @@
-(require 'multiple-cursors)
-;; (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-;; mc/mark-all-like -this
-(global-set-key (kbd "C-M->") 'mc/mark-all-like-this-dwim)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "s-<mouse-1>") 'mc/add-cursor-on-click)
-(setq mc/list-file (concat user-emacs-directory ".mc-lists.el"))
-;; "C-'" while multiple-cursor-mode
-(mc-hide-unmatched-lines-mode -1)
+(when (or
+       peccu-p
+       win-env-p
+       ;; wsl-p
+       )
+  (require 'multiple-cursors)
+  ;; (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+  ;; mc/mark-all-like -this
+  (global-set-key (kbd "C-M->") 'mc/mark-all-like-this-dwim)
+  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "s-<mouse-1>") 'mc/add-cursor-on-click)
+  (setq mc/list-file (concat user-emacs-directory ".mc-lists.el"))
+  ;; "C-'" while multiple-cursor-mode
+  (mc-hide-unmatched-lines-mode -1)
+  )
