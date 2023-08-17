1dafa6b39e4b231a9d22c4c7255f7c4a6977733d
peccu <peccul@gmail.com>
2023-08-17 17:34:12 +0900

    merged editorconfig.el

---
+++ conf.d/editorconfig.el
@@ -0,0 +1,24 @@
+(when (or
+       peccu-p
+       ;; win-env-p
+       ;; wsl-p
+       )
+  ;; (setq edconf-exec-path "/usr/local/bin/editorconfig")
+  ;; (load "editorconfig")
+  (require 'editorconfig)
+  (setq edconf-indentation-alist
+        (cons edconf-indentation-alist
+              '((vue-mode js-indent-level
+                          (web-mode-indent-style . (lambda (size) 2))
+                          web-mode-markup-indent-offset
+                          web-mode-css-indent-offset
+                          web-mode-code-indent-offset
+                          web-mode-script-padding
+                          web-mode-style-padding
+                          css-indent-offset
+                          )
+                (js-mode js-indent-level)
+                (javascript-mode js-indent-level)
+                )))
+  (editorconfig-mode 1)
+  )
