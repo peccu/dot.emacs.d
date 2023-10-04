e97bd30de1769568577ffaa595cae5cc442bcd0c
peccu <peccul@gmail.com>
2023-10-04 14:02:24 +0900

    add treesitter conf in comment

---
+++ conf.d/treesitter.el
@@ -0,0 +1,13 @@
+(when (or
+       peccu-p
+       win-env-p
+       wsl-p
+       )
+  ;; packages version
+  ;; (require-with-install 'xxx-mode)
+  ;; submodule version
+  ;; (add-submodule-to-load-path "git/xxx-mode")
+  ;; (require 'xxx-mode)
+
+  ;; https://github.com/aadi58002/stow-dotfiles/blob/main/.config/emacs/init.org#tree-sitter
+  )
