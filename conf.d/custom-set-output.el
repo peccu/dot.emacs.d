e45a1ce613065003b30d103140de6838c3c8bc62
peccu <peccul@gmail.com>
2023-10-04 13:58:09 +0900

    change custom-set output file

---
+++ conf.d/custom-set-output.el
@@ -0,0 +1,26 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ahs-default-range 'ahs-range-whole-buffer)
+ '(anzu-deactivate-region t)
+ '(anzu-mode-lighter "")
+ '(anzu-replace-to-string-separator " => ")
+ '(anzu-search-threshold 1000)
+ '(lsp-tailwindcss-add-on-mode t)
+ '(magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1)
+ '(package-archives
+   '(("org" . "https://orgmode.org/elpa/")
+     ("melpa" . "https://melpa.org/packages/")
+     ("gnu" . "https://elpa.gnu.org/packages/")))
+ '(package-selected-packages
+   '(ag alchemist all-the-icons anzu auto-highlight-symbol blackout bm color-moccur color-theme-modern company company-box dmacro docker docker-compose-mode dockerfile-mode editorconfig eev el-get elixir-mode emmet-mode exec-path-from-shell find-file-in-project git-gutter-fringe go-mode google-this guide-key helm-ag helm-c-yasnippet helm-emmet helm-files helm-swoop hydra js2-mode leaf lsp-mode magit magit-section markdown-mode markdown-preview-eww markdown-preview-mode markdown-toc multiple-cursors nyan-mode open-junk-file powershell quickrun rg rust-mode sequential-command session switch-window tide ts-comint typescript-mode use-package vdiff web-mode window-numbering yafolding yasnippet zoom-window))
+ '(session-use-package t nil (session))
+ '(yas-prompt-functions '(my-yas/prompt)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(lsp-ui-doc-background ((t (:background "black")))))
