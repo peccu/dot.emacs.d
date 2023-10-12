35440d6a904fc9ed33883e45c6c13e0b7450b1de
peccu <peccul@gmail.com>
2023-10-12 05:36:04 +0000

    add template

---
+++ conf.d/_template.el
@@ -0,0 +1,12 @@
+(when (or
+       peccu-p
+       win-env-p
+       wsl-p
+       )
+  ;; packages version
+  ;; (require-with-install 'xxx-mode)
+  ;; submodule version
+  ;; (add-submodule-to-load-path "git/xxx-mode")
+  ;; (require 'xxx-mode)
+
+  )
