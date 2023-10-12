9dd5162016e56a099e36f05b4bc3e05fed88a70b
peccu <peccul@gmail.com>
2023-10-12 05:40:25 +0000

    update smartchr conf

--- conf.d/smartchr.el
+++ conf.d/smartchr.el
@@ -5,7 +5,7 @@
        )
   (add-submodule-to-load-path "git/emacs-smartchr/")
   (require 'smartchr)
-  (global-set-key (kbd "=") (smartchr '(" = " "=" " => " " == " " != ")))
+  ;; (global-set-key (kbd "=") (smartchr '(" = " "=" " => " " == " " != ")))
 
   ;; (define-key org-mode-map (kbd "$") (smartchr '(" $`!!'$ " "#+begin_latex
   ;;   % \\setcounter{equation}{35}
@@ -137,31 +137,31 @@
   ;;                    (delete-backward-char 1)))
   ;;    ))
 
-  ;; (defun my-smartchr-keybindings ()
-  ;;   ;; !! がカーソルの位置
-  ;;   (local-set-key (kbd "=") (smartchr '("=" " = " " == ")))
-  ;;   (local-set-key (kbd "+") (smartchr '("+" " + " "++" " += ")))
-  ;;   (local-set-key (kbd "-") (smartchr '("-" " - " "--" " -= ")))
-  ;;   (local-set-key (kbd "(") (smartchr '("(`!!')" "(")))
-  ;;   (local-set-key (kbd "[") (smartchr '("[`!!']" "[[`!!']]" "[")))
-  ;;   (local-set-key (kbd "{") (smartchr '("{" "{`!!'}" my-smartchr-braces)))
-  ;;   ;;バッククォート
-  ;;   (local-set-key (kbd "`") (smartchr '("\``!!''" "\`")))
-  ;;   ;;ダブルクォーテーション
-  ;;   (local-set-key (kbd "\"") (smartchr '("\"`!!'\"" "\"")))
-  ;;   ;;シングルクォート
-  ;;   (local-set-key (kbd "\'") (smartchr '("\'`!!'\'" "\'")))
-  ;;   (local-set-key (kbd ">") (smartchr '(">" " > " " => " " >> ")))
-  ;;   (local-set-key (kbd "<") (smartchr '("<" " < " " << " "<`!!'>")))
-  ;;   (local-set-key (kbd ",") (smartchr '(", " ",")))
-  ;;   (local-set-key (kbd ".") (smartchr '("." " . ")))
-  ;;   (local-set-key (kbd "?") (smartchr '("?" "? `!!' " "<?`!!'?>")))
-  ;;   (local-set-key (kbd "!") (smartchr '("!" " != ")))
-  ;;   (local-set-key (kbd "&") (smartchr '("&" " && ")))
-  ;;   (local-set-key (kbd "|") (smartchr '("|" " || ")))
-  ;;   (local-set-key (kbd "/") (smartchr '("/" "/* `!!' */" my-smartchr-comment)))
-  ;;   (local-set-key (kbd ";") (smartchr '(";" my-smartchr-semicolon)))
-  ;;   )
+  (defun my-smartchr-keybindings ()
+    ;; !! がカーソルの位置
+    (local-set-key (kbd "=") (smartchr '("=" " = " " => " " == " " != ")))
+    ;; (local-set-key (kbd "+") (smartchr '("+" " + " "++" " += ")))
+    ;; (local-set-key (kbd "-") (smartchr '("-" " - " "--" " -= ")))
+    ;; (local-set-key (kbd "(") (smartchr '("(`!!')" "(")))
+    ;; (local-set-key (kbd "[") (smartchr '("[`!!']" "[[`!!']]" "[")))
+    ;; (local-set-key (kbd "{") (smartchr '("{" "{`!!'}" my-smartchr-braces)))
+    ;; ;;バッククォート
+    ;; (local-set-key (kbd "`") (smartchr '("\``!!''" "\`")))
+    ;; ;;ダブルクォーテーション
+    ;; (local-set-key (kbd "\"") (smartchr '("\"`!!'\"" "\"")))
+    ;; ;;シングルクォート
+    ;; (local-set-key (kbd "\'") (smartchr '("\'`!!'\'" "\'")))
+    ;; (local-set-key (kbd ">") (smartchr '(">" " > " " => " " >> ")))
+    ;; (local-set-key (kbd "<") (smartchr '("<" " < " " << " "<`!!'>")))
+    ;; (local-set-key (kbd ",") (smartchr '(", " ",")))
+    ;; (local-set-key (kbd ".") (smartchr '("." " . ")))
+    ;; (local-set-key (kbd "?") (smartchr '("?" "? `!!' " "<?`!!'?>")))
+    ;; (local-set-key (kbd "!") (smartchr '("!" " != ")))
+    ;; (local-set-key (kbd "&") (smartchr '("&" " && ")))
+    ;; (local-set-key (kbd "|") (smartchr '("|" " || ")))
+    ;; (local-set-key (kbd "/") (smartchr '("/" "/* `!!' */" my-smartchr-comment)))
+    ;; (local-set-key (kbd ";") (smartchr '(";" my-smartchr-semicolon)))
+    )
 
   ;; (defun my-smartchr-keybindings-lisp ()
   ;;   (local-set-key (kbd "(") (smartchr '("(`!!')" "(")))
@@ -175,19 +175,18 @@
   ;;   (local-set-key (kbd "@") (smartchr '("@\"`!!'\"" "@")))
   ;;   (local-set-key (kbd "[") (smartchr '("[`!!']" "[[`!!']]" "[[[`!!']]]" "["))))
 
-  ;; (dolist (hook (list
-  ;;                'c-mode-common-hook
-  ;;                'c++-mode-hook
-  ;;                'php-mode-hook
-  ;;                'ruby-mode-hook
-  ;;                'cperl-mode-hook
-  ;;                'javascript-mode-hook
-  ;;                'js-mode-hook
-  ;;                'js2-mode-hook
-  ;;                'text-mode-hook
-  ;;                ))
-  ;;   (add-hook hook 'my-smartchr-keybindings))
-  ;; (add-hook 'js2-mode-hook 'my-smartchr-keybindings)
+  (dolist (hook (list
+                 'c-mode-common-hook
+                 'c++-mode-hook
+                 'php-mode-hook
+                 'ruby-mode-hook
+                 'cperl-mode-hook
+                 'javascript-mode-hook
+                 'js-mode-hook
+                 'js2-mode-hook
+                 'text-mode-hook
+                 ))
+    (add-hook hook 'my-smartchr-keybindings))
 
   ;; (dolist (hook (list
   ;;                'lisp-mode-hook
