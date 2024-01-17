49f7814edbf0f42c92652a7e05d6db468f6b1da4
peccu <peccul@gmail.com>
2024-01-17 00:39:43 +0000

    fix popwin weired behavior

--- conf.d/popwin.el
+++ conf.d/popwin.el
@@ -7,6 +7,31 @@
   ;; (setq display-buffer-function 'popwin:display-buffer)
   ;; obsolete from 24.3
   ;; change to this (https://github.com/m2ym/popwin-el/commit/b6ac46b65acb15894ae9ae34686945369d798e93)
+
+  ;; fix some issue
+  ;; https://github.com/emacsorphanage/popwin/issues/147
+  (defun popwin:window-config-tree-1 (node)
+    "Not documented (NODE)."
+    (if (windowp node)
+        (list 'window
+              node
+              (window-buffer node)
+              (popwin:window-point node)
+              (window-start node)
+              (window-edges node)
+              (eq (selected-window) node)
+              (window-dedicated-p node))
+      (cl-destructuring-bind (dir edges . windows) node
+        (append (list dir edges)
+                (cl-loop for window in windows
+                         ;; revert the commit https://github.com/emacsorphanage/popwin/commit/c5c98318fa9ceba6f9b1e4c57c5ac0514f057176
+                         ;; unless (or (and (windowp window)
+                         ;;                 (window-parameter window 'window-side))
+                         ;;            (not (windowp window)))
+                         unless (and (windowp window)
+                                     (window-parameter window 'window-side))
+                         collect (popwin:window-config-tree-1 window))))))
+
   (popwin-mode 1)
 
   (push '("*grep*" :height 80) popwin:special-display-config)
