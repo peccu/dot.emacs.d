e9a6f5e26650cea18f89ac188683411dee92f37f
peccu <peccul@gmail.com>
2024-01-22 20:47:01 +0900

    add missing package installing

--- conf.d/popup-color-at-point.el
+++ conf.d/popup-color-at-point.el
@@ -3,6 +3,7 @@
        win-env-p
        wsl-p
        )
+  (require-with-install 'popup)
   (load (concat user-emacs-directory "lisp/popup-color-at-point.el"))
   (global-set-key (kbd "C-c c") 'popup-color-at-point)
   )
