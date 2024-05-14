709ec9613a8304882bfa71da1bf921f94d286d5e
peccu <peccul@gmail.com>
2024-05-14 09:31:34 +0000

    update highlight char config

--- conf.d/highlight-chars.el
+++ conf.d/highlight-chars.el
@@ -1,7 +1,7 @@
 (when (or
        peccu-p
        win-env-p
-       ;; wsl-p
+       wsl-p
        )
   (require 'highlight-chars)
   ;; in highlight-chars-autoloads.el
@@ -9,75 +9,28 @@
   (add-hook 'font-lock-mode-hook 'hc-highlight-tabs)
   (add-hook 'font-lock-mode-hook 'hc-highlight-trailing-whitespace)
 
-  ;; ---------------- wide space
-
-;;;###autoload
-  (defface hc-wide-space '((t (:background "PaleGreen")))
-    "*Face for highlighting wide spaces (`　') in Font-Lock mode."
+  ;; set my highlight target chars
+  (setq my-hc-highlight-chars '(
+                                ;; wide space
+                                ;; (U+3000 : char code 12288)
+                                "　"
+                                ;; zero width spaces
+                                ;; (U+200b : char code 8203)
+                                "​"
+                                ;; &nbsp; spaces
+                                ;; (U+00A0 : char code 160)
+                                " "
+                                ))
+  ;; set face if you want to change
+  (defface my-hc-highlight-face '((t (:background "PaleGreen")))
+    "*Face for highlighting chars in `my-hc-highlight-chars' in Font-Lock mode."
     :group 'Highlight-Characters :group 'faces)
 
-  (defvar hc-highlight-wide-space-p nil
-    "Non-nil means font-lock mode highlights wide space characters (`　').")
-
-;;;###autoload
-  (defalias 'toggle-highlight-wide-space 'hc-toggle-highlight-wide-space)
-;;;###autoload
-  (defun hc-toggle-highlight-wide-space (&optional msgp)
-    "Toggle highlighting of Wide Space, using face `hc-wide-space'."
-    (interactive "p")
-    (setq hc-highlight-wide-space-p  (not hc-highlight-wide-space-p))
-    (if hc-highlight-wide-space-p
-        (add-hook 'font-lock-mode-hook 'hc-highlight-wide-space)
-      (remove-hook 'font-lock-mode-hook 'hc-highlight-wide-space)
-      (hc-dont-highlight-wide-space))
-    (font-lock-mode) (font-lock-mode)
-    (when msgp
-      (message "Wide Spaces highlighting is now %s" (if hc-highlight-wide-space-p "ON" "OFF"))))
-
-  (defun hc-highlight-wide-space ()
-    "Highlight wide spaces (`　')."
-    (font-lock-add-keywords nil '(("[　]+" (0 'hc-wide-space t))) 'APPEND))
-
-  (defun hc-dont-highlight-wide-space ()
-    "Do not highlight wide spaces (`　')."
-    (when (fboundp 'font-lock-remove-keywords)
-      (font-lock-remove-keywords nil '(("[　]+" (0 'hc-wide-space t))))))
-
-  (add-hook 'font-lock-mode-hook 'hc-highlight-wide-space)
-
-  ;; ---------------- zero width spaces
-
-;;;###autoload
-  (defface hc-zero-width-space '((t (:background "PaleGreen")))
-    "*Face for highlighting zero width spaces (`​') in Font-Lock mode."
-    :group 'Highlight-Characters :group 'faces)
-
-  (defvar hc-highlight-zero-width-space-p nil
-    "Non-nil means font-lock mode highlights zero width spaces characters (`​').")
-
-;;;###autoload
-  (defalias 'toggle-highlight-zero-width-space 'hc-toggle-highlight-zero-width-space)
-;;;###autoload
-  (defun hc-toggle-highlight-zero-width-space (&optional msgp)
-    "Toggle highlighting of zero width spaces, using face `hc-zero-width-space'."
-    (interactive "p")
-    (setq hc-highlight-zero-width-space-p  (not hc-highlight-zero-width-space-p))
-    (if hc-highlight-zero-width-space-p
-        (add-hook 'font-lock-mode-hook 'hc-highlight-zero-width-space)
-      (remove-hook 'font-lock-mode-hook 'hc-highlight-zero-width-space)
-      (hc-dont-highlight-zero-width-space))
-    (font-lock-mode) (font-lock-mode)
-    (when msgp
-      (message "zero width spaces highlighting is now %s" (if hc-highlight-zero-width-space-p "ON" "OFF"))))
-
-  (defun hc-highlight-zero-width-space ()
-    "Highlight zero width spaces (`​')."
-    (font-lock-add-keywords nil '(("[​]+" (0 'hc-zero-width-space t))) 'APPEND))
-
-  (defun hc-dont-highlight-zero-width-space ()
-    "Do not highlight zero width spaces (`​')."
-    (when (fboundp 'font-lock-remove-keywords)
-      (font-lock-remove-keywords nil '(("[​]+" (0 'hc-zero-width-space t))))))
-
-  (add-hook 'font-lock-mode-hook 'hc-highlight-zero-width-space)
+  ;; enable highlight
+  ;; this add hook to font-lock-mode-hook
+  ;; (hc-highlight-chars my-hc-highlight-chars 'my-hc-highlight-face nil t)
+  (hc-highlight-chars my-hc-highlight-chars 'hc-other-char nil t) ;HotPink
+  ;; disable highlight = remove-hook
+  ;; (hc-highlight-chars my-hc-highlight-chars 'my-hc-highlight-face t t)
+  ;; (hc-highlight-chars my-hc-highlight-chars 'hc-other-char t t)
   )
