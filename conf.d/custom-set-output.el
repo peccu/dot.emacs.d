57f1a2cbe54e0e2f71d06296b2daa8ba1bfce6ba
peccu <peccul@gmail.com>
2025-07-27 10:37:22 +0900

    extract org-babel config and add company-org-block

--- conf.d/custom-set-output.el
+++ conf.d/custom-set-output.el
@@ -21,7 +21,7 @@
      ("melpa" . "https://melpa.org/packages/")
      ("gnu" . "https://elpa.gnu.org/packages/")))
  '(package-selected-packages
-   '(ag alchemist all-the-icons anzu auto-highlight-symbol blackout bm color-moccur color-theme-modern company company-box dart-mode dmacro docker docker-compose-mode dockerfile-mode editorconfig eev el-get elixir-mode emmet-mode ess exec-path-from-shell find-file-in-project flutter git-gutter-fringe go-mode google-this guide-key helm-ag helm-c-yasnippet helm-emmet helm-files helm-swoop hydra js2-mode leaf lsp-mode magit magit-section markdown-mode markdown-preview-eww markdown-preview-mode markdown-toc multiple-cursors nyan-mode open-junk-file powershell quickrun rg rust-mode sequential-command session switch-window tide ts-comint typescript-mode use-package vdiff web-mode window-numbering yafolding yasnippet zoom-window))
+   '(ag alchemist all-the-icons anzu auto-highlight-symbol blackout bm color-moccur color-theme-modern company company-box company-org-block corfu dart-mode dmacro docker docker-compose-mode dockerfile-mode editorconfig eev el-get elixir-mode emmet-mode ess exec-path-from-shell find-file-in-project flutter git-gutter-fringe go-mode google-this guide-key helm-ag helm-c-yasnippet helm-emmet helm-files helm-swoop hydra js2-mode leaf lsp-mode magit magit-section markdown-mode markdown-preview-eww markdown-preview-mode markdown-toc multiple-cursors nyan-mode open-junk-file powershell quickrun rg rust-mode sequential-command session switch-window tide ts-comint typescript-mode use-package vdiff web-mode window-numbering yafolding yasnippet zoom-window))
  '(session-use-package t nil (session))
  '(shell-pop-full-span t)
  '(shell-pop-restore-window-configuration t)
